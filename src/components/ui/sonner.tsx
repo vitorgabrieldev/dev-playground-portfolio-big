8cf3b70d17f79ed08808d493a12d1764e36298ff
vitorgabrieldev <vitorgabrieldeoliveiradev@gmail.com>
2025-06-18 22:37:41 -0300

    fix: :bug:

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,23 +1,21 @@
 import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { Toaster as SonnerToaster, type ToasterProps } from "sonner"
 
-const Toaster = ({ ...props }: ToasterProps) => {
+const CustomToaster = (props: ToasterProps) => {
   const { theme = "system" } = useTheme()
 
   return (
-    <Sonner
+    <SonnerToaster
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
+      style={{
+        "--normal-bg": "var(--popover)",
+        "--normal-text": "var(--popover-foreground)",
+        "--normal-border": "var(--border)",
+      } as React.CSSProperties}
       {...props}
     />
   )
 }
 
-export { Toaster }
+export { CustomToaster }
